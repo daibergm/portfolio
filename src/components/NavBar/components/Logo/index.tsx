f7e5dcb76fcf01f3e91727e4eb8539cbee93b6b9
Daiber <44331187+daibergm@users.noreply.github.com>
2023-03-16 22:19:43 -0500

    fix: add static folder (#7)

--- src/components/NavBar/components/Logo/index.tsx
+++ src/components/NavBar/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { LogoProps } from './types';
 
 function Logo({ width, height }: LogoProps) {
-  return <Image src="/../public/assets/navLogo.png" alt="logo" width={width} height={height} />;
+  return <Image src="/static/assets/navLogo.png" alt="logo" width={width} height={height} />;
 }
 
 export default Logo;
