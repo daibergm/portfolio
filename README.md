2f9ec3b43aa6525d0d7085c751bad2bc0a62651c
Daiber <44331187+daibergm@users.noreply.github.com>
2023-03-16 22:03:01 -0500

    feat: create nav-bar component (#6)

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-## Getting Started
-
-## Description
-
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+<h1 align="center">Portfolio Web App</h1>
 
 ## Tech Stack
 
@@ -13,6 +9,21 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 - [Jest](https://jestjs.io/)
 - [React Testing Library](https://testing-library.com/)
 - [Husky](https://typicode.github.io/husky/)
+- [TailwindCss](https://tailwindcss.com/docs/guides/nextjs)
+- [React-Icons](https://react-icons.github.io/react-icons/)
+
+## Features Status
+
+- [Boilerplate](#) : :heavy_check_mark:
+- [Desktop NavBar](#) : :heavy_check_mark:
+- [Mobile NavBar](#) : :heavy_check_mark:
+- [Home Page](#) : :warning:
+- [About Page](#) : :warning:
+- [Skills Page](#) : :warning:
+- [Projects Page](#) : :warning:
+- [Contact Page](#) : :warning:
+- [Dark Mode](#) : :warning:
+- [i18n](#) : :warning:
 
 ## Run Locally
 
@@ -20,16 +31,12 @@ Clone the project
 
 ```bash
   git clone https://github.com/daibergm/nextjs-template.git
-  or
-  git clone https://github.com/daibergm/nextjs-template.git project-name
 ```
 
 Go to the project directory
 
 ```bash
-  cd nextjs-template
-  or
-  cd project-name
+  cd portfolio
 ```
 
 Install dependencies
@@ -65,24 +72,3 @@ To run tests, run the following command
 ```bash
   yarn test
 ```
-
-You can start editing the page by modifying `src/pages/index.page.ts`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `src/pages/api/hello.api.ts`.
-
-The `src/pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,93 @@
-import React from 'react';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import Logo from './components/Logo';
+import { menus } from './constants';
 
 function NavBar() {
-  return <h1>NavBar</h1>;
+  const [showNav, setShowNav] = useState(false);
+
+  const handleShowNav = () => setShowNav(!showNav);
+
+  const renderMenu = () =>
+    menus.map(({ name }, index) => (
+      <Link href="/" key={Math.random()}>
+        <li className="text-sm hover:bg-[#709dff] p-2" data-testid={`menu-item-${index}`}>
+          {name}
+        </li>
+      </Link>
+    ));
+
+  return (
+    // Container
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      {/* Desktop NavBar */}
+      <div className="flex justify-between items-center w-full h-full px-4 2xl:px-16">
+        <Logo width={125} height={50} />
+        <div>
+          <ul className="hidden md:flex uppercase">{renderMenu()}</ul>
+          <div data-testid="desktop-nav-toggle-btn" onClick={handleShowNav} className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      {/* Mobile NavBar */}
+      <div
+        data-testid="mobile-nav"
+        className={showNav ? 'fixed left-0 top-0 w-full h-screen bg-black/70 md:hidden' : ''}
+      >
+        {/* Sub container */}
+        <div
+          className={
+            showNav
+              ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] h-screen bg-[#ecf0f3] p-4 ease-in duration-500'
+              : 'fixed left-[-100%] top-0 p-4 ease-in duration-500'
+          }
+        >
+          {/* Header */}
+          <div className="flex justify-between px-2">
+            <Logo width={87} height={35} />
+            <div
+              data-testid="mobile-nav-close-btn"
+              onClick={handleShowNav}
+              className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer flex items-center"
+            >
+              <AiOutlineClose size={20} />
+            </div>
+          </div>
+          {/* Slogan */}
+          <div className="border-b border-gray-300 my-4 px-2">
+            <p className="py-4">{`Let's build something legendary together`}</p>
+          </div>
+          {/* Menu */}
+          <div>
+            <ul className="uppercase">{renderMenu()}</ul>
+          </div>
+          {/* Footer */}
+          <div className="px-2  pt-40">
+            <p className="uppercase tracking-widest text-[#5651e5]">{`let's connect`}</p>
+            {/* Social icons */}
+            <div className="flex py-4 w-full">
+              <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-200 mr-2">
+                <FaLinkedinIn size={20} />
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-200 mr-2">
+                <FaGithub size={20} />
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-200 mr-2">
+                <AiOutlineMail size={20} />
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-200 mr-2">
+                <BsFillPersonLinesFill size={20} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
