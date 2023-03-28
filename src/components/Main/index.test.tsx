0d595086dbc252e8f105f14b85440ab4c312492c
Daiber <44331187+daibergm@users.noreply.github.com>
2023-03-28 14:18:13 -0500

    feat: skills section (#12)

--- src/components/Main/index.test.tsx
+++ src/components/Main/index.test.tsx
@@ -5,13 +5,13 @@ import Main from '.';
 describe('Main', () => {
   it('renders correctly', async () => {
     const { getByText } = render(<Main />);
-    expect(getByText(`Hi, I'm`)).toBeTruthy();
-    expect(getByText(`Daiber`)).toBeTruthy();
-    expect(getByText(`A Front-End Developer`)).toBeTruthy();
+    expect(getByText(`Hi, I'm`)).toBeInTheDocument();
+    expect(getByText(`Daiber`)).toBeInTheDocument();
+    expect(getByText(`A Front-End Developer`)).toBeInTheDocument();
     expect(
       getByText(
         `I'm a front-end developer specializing in building exceptional digital experiences. Currently, I'm focussed on building responsive web apps and mobile apps, while learning other technologies.`
       )
-    ).toBeTruthy();
+    ).toBeInTheDocument();
   });
 });
