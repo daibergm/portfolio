a8f6946100e56a3a977ef187126710c3782c8ae4
Daiber Gonzalez <daiber.gonzalez@jkmoving.com>
2024-04-02 22:45:38 -0500

    feat: upgrade libs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
   darkMode: 'class',
   theme: {
     container: {
